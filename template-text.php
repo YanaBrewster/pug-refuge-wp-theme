fa1535e77b63b24f2b801de8be4e93bb6028eec7
YanaBrewster <yana.brewster@gmail.com>
2020-06-11 13:14:58 +1200

    page template for text added and CSS styles

---
+++ template-text.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Text Page */ ?>
+<!-- Header -->
+<?php get_header(); ?>
+
+<section class="mx-auto mx-2 my-2 mb-5">
+
+      <h2 class="myHeadings text-center my-5"> <?php the_title(); ?></h2>
+
+<div class="text-box mx-auto">
+    <?php get_template_part('includes/section','content'); ?>
+</div>
+
+
+</section>
+<!-- Footer -->
+<?php get_footer(); ?>
